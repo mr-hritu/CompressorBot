c9b37984e6c0f40584a7ec990d73835aa9fa3ecb
mr-hritu <83459565+mr-hritu@users.noreply.github.com>
2023-06-08 10:31:31 +0530

    Update config.py

--- helper/config.py
+++ helper/config.py
@@ -15,11 +15,11 @@
 from . import *
 
 try:
-    APP_ID = config("APP_ID", cast=int)
-    API_HASH = config("API_HASH")
+    APP_ID = config("APP_ID", default=29616312 , cast=int)
+    API_HASH = config("API_HASH" , default="dd1a05ab4c47a5a037cc067cf4bded27")
     BOT_TOKEN = config("BOT_TOKEN")
-    OWNER = config("OWNER_ID", default=1322549723, cast=int)
-    LOG = config("LOG_CHANNEL", cast=int)
+    OWNER = config("OWNER_ID", default=5190902724, cast=int)
+    LOG = config("LOG_CHANNEL", default=-1001971871116, cast=int)
 except Exception as e:
     LOGS.info("Environment vars Missing")
     LOGS.info("something went wrong")
